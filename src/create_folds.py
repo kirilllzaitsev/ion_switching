678b6066b0abebae67ae866bed3156a8e2d2bfb3
cyberpunkt317 <cyberpank317@gmail.com>
2020-03-28 11:50:51 +0200

    First commit

---
+++ src/create_folds.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from sklearn import model_selection
+
+if __name__ == "__main__":
+    df = pd.read_csv('input/train.csv')
+    df['kfold'] = -1
+
+    df = df.sample(frac=1).reset_index(drop=True)
